7161a52051cdf1aa330e64860cb27a34f5d8ca46
andreytlm <voronov1992310@gmail.com>
2025-04-09 15:57:33 +0300

    Задача 1 к Уроку 1

--- src/main/kotlin/KSL_1/task_1.kt
+++ src/main/kotlin/KSL_1/task_1.kt
@@ -2,8 +2,6 @@ package KSL_1
 
 
 fun main() {
-    //количество заказов
     val numberOfOrders: Int = 75
-    //текст с благодарностью за покупку (текст на твое усмотрение).
     val purchaseThankYou: String = "Thank you for order in our store!"
 }
--- src/main/kotlin/lesson_1/lesson1_test.kt
+++
@@ -1,9 +0,0 @@
-package lesson_1
-
-//пробую закинуть на пул ревью
-fun main() {
-    val i = 15
-
-    println(i)
-
-}
