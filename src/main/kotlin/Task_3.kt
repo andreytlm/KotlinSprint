e5a5207b992c359ef7410a38af50164d79152254
andreytlm <voronov1992310@gmail.com>
2025-04-09 16:23:55 +0300

    Задача 3 к Уроку 1

---
+++ src/main/kotlin/Task_3.kt
@@ -0,0 +1,13 @@
+package KotlinSprint_Task3
+
+
+fun main() {
+    val year: Int = 1961
+    var hour = "09"
+    var minute = "07"
+    println("Год полета - $year")
+    println("Взлет - $hour:$minute")
+    hour = "10"
+    minute = "55"
+    println("Посадка - $hour:$minute")
+}
