1b076d86bfcbb9bad0e456a9a0f29ce583cd7c98
andreytlm <voronov1992310@gmail.com>
2025-04-09 15:15:04 +0300

    Задача 3 к Уроку 1

---
+++ src/main/kotlin/KSL_1/task_3.kt
@@ -0,0 +1,14 @@
+package KSL_1
+
+
+fun main() {
+    val year: Int
+    year = 1961
+    var hour = "09"
+    var minute = "07"
+    println("Год полета - $year")
+    println("Взлет - $hour:$minute")
+    hour = "10"
+    minute = "55"
+    println("Посадка - $hour:$minute")
+}
