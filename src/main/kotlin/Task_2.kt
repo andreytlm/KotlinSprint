71c7b92289d371561732ef911a3ea700ac45277e
andreytlm <voronov1992310@gmail.com>
2025-04-09 16:19:02 +0300

    Задача 2 к Уроку 1

---
+++ src/main/kotlin/Task_2.kt
@@ -0,0 +1,13 @@
+package KotlinSprint_Task2
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val purchaseThankYou: String = "Thank you for order in our store!"
+    println(numberOfOrders)
+    println(purchaseThankYou)
+
+    var numberOfStaff = 2000
+    //println("Numbers of staff is $numberOfStaff")
+    numberOfStaff--
+    println("Numbers of staff is $numberOfStaff")
+}
