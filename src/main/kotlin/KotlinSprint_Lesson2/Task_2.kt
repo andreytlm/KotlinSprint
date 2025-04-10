f98055f04d11f77da4775ed1f384e8b452f62ca8
andreytlm <voronov1992310@gmail.com>
2025-04-10 12:53:56 +0300

    Задача 2 к Уроку 2 Решение

---
+++ src/main/kotlin/KotlinSprint_Lesson2/Task_2.kt
@@ -0,0 +1,15 @@
+package org.example.KotlinSprint_Lesson2
+
+fun main() {
+    val numberOfStaff = 50
+    val staffSalary = 50000
+    val numberOfIntern = 30
+    val internSalary = 20000
+    val expenditureOfStaff = numberOfStaff * staffSalary
+    val expenditureOfAllStaff = (numberOfIntern * internSalary) + expenditureOfStaff
+    val averageSalaryForAllStaff = expenditureOfAllStaff / (numberOfStaff+numberOfIntern)
+
+    println(expenditureOfStaff)
+    println(expenditureOfAllStaff)
+    println(averageSalaryForAllStaff)
+}
