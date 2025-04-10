4c145d13080132f885aeb85ca05e5e2c98860f8d
andreytlm <voronov1992310@gmail.com>
2025-04-10 15:56:45 +0300

    Задача 5 к Уроку 1 исправление

--- src/main/kotlin/Task_5.kt
+++ src/main/kotlin/Task_5.kt
@@ -1,14 +1,11 @@
 package KotlinSprint_Task5
 
 fun main() {
-    val secondsInSpace: Short = 6480
-    val hours = secondsInSpace/3600
-    val remainderSecondsFromHours = secondsInSpace % 3600
-    val minutes = remainderSecondsFromHours / 60
-    val remainderSecondsFromMinutes = secondsInSpace % 60
-
-    val formatData = "%02d:%02d:%02d".format(hours, minutes, remainderSecondsFromMinutes)
-
-    println(formatData)
-
+    println("%02d:%02d:%02d".format(hours, minutes, seconds))
 }
+
+val secondsInSpace: Short = 6480
+val hours = secondsInSpace / 3600
+val remainderSecondsFromHours = secondsInSpace % 3600
+val minutes = remainderSecondsFromHours / 60
+val seconds = secondsInSpace % 60
