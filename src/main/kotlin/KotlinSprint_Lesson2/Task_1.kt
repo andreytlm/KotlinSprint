cae6e82c8015de762dc6b54d8aaaaa9de8b441c2
andreytlm <voronov1992310@gmail.com>
2025-04-10 16:03:49 +0300

    Задача 1 к Уроку 2 стилистическое исправление

--- src/main/kotlin/KotlinSprint_Lesson2/Task_1.kt
+++ src/main/kotlin/KotlinSprint_Lesson2/Task_1.kt
@@ -1,12 +1,12 @@
 package org.example.KotlinSprint_Lesson2
 
 fun main() {
-    val numberOfStudents: Float = 4.00f
-    val gradeStudent1: Float = 3.00f
-    val gradeStudent2: Float = 4.00f
-    val gradeStudent3: Float = 3.00f
-    val gradeStudent4: Float = 5.00f
-    val averageScore: Float = ((gradeStudent1+gradeStudent2+gradeStudent3+gradeStudent4)/numberOfStudents)
-    println("Средний бал по английскому языку - $averageScore")
+    val numberOfStudents = 4.00f
+    val gradeStudent1 = 3.00f
+    val gradeStudent2 = 4.00f
+    val gradeStudent3 = 3.00f
+    val gradeStudent4 = 5.00f
+    val averageScore = ((gradeStudent1 + gradeStudent2 + gradeStudent3 + gradeStudent4) / numberOfStudents)
 
+    println("Средний бал по английскому языку - $averageScore")
 }
