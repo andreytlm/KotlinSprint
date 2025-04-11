f721e94569529e4022332f4b95a5795029b67532
andreytlm <voronov1992310@gmail.com>
2025-04-11 11:19:03 +0300

    Задача 5 к Уроку 1 убраны магические числа

--- src/main/kotlin/Task_5.kt
+++ src/main/kotlin/Task_5.kt
@@ -1,11 +1,16 @@
 package KotlinSprint_Task5
 
 fun main() {
+    val secondsInSpace: Short = 6480
+    val hours = secondsInSpace / SECONDS_IN_HOUR
+    val remainderSecondsFromHours = secondsInSpace % SECONDS_IN_HOUR
+    val minutes = remainderSecondsFromHours / SECONDS_IN_MINUTE
+    val seconds = secondsInSpace % SECONDS_IN_MINUTE
+
     println("%02d:%02d:%02d".format(hours, minutes, seconds))
 }
 
-val secondsInSpace: Short = 6480
-val hours = secondsInSpace / 3600
-val remainderSecondsFromHours = secondsInSpace % 3600
-val minutes = remainderSecondsFromHours / 60
-val seconds = secondsInSpace % 60
+const val SECONDS_IN_MINUTE = 60
+const val SECONDS_IN_HOUR = 3600
+
+
