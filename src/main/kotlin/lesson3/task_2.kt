69ae37bb3537080109da6c88d18a9fc3474375d1
andreytlm <voronov1992310@gmail.com>
2025-04-14 12:46:30 +0300

    Задача 2 к Уроку 3 решение

---
+++ src/main/kotlin/lesson3/task_2.kt
@@ -0,0 +1,13 @@
+package lesson3
+
+fun main() {
+    val personalFirstSurname = "Андреева"
+    val personalName = "Татьяна"
+    val personalPatronymic = "Сергеевна"
+    val personalAfterWeddingSurname = "Сидорова"
+    val age20 = 20
+    val age22 = 22
+
+    println("$personalFirstSurname $personalName $personalPatronymic возраст = $age20")
+    println("$personalAfterWeddingSurname $personalName $personalPatronymic возраст = $age22")
+}
