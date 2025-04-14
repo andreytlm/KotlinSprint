1c79f77e5778ff7b1749bed7d9e8ddb55944222a
andreytlm <voronov1992310@gmail.com>
2025-04-14 11:09:45 +0300

    Задача 5 к Уроку 2 решение

--- src/main/kotlin/ks2/task_5.kt
+++ src/main/kotlin/ks2/task_5.kt
@@ -3,5 +3,9 @@ package ks2
 fun main() {
     val clientMoney = 70000
     val interestRate = 16.7
+    val durationOfDepositYears = 20
+    val depositIncome = ((clientMoney * interestRate * durationOfDepositYears) / TO_PROCENT) + clientMoney
 
+    println("%.3f".format(depositIncome))
 }
+const val TO_PROCENT = 100
